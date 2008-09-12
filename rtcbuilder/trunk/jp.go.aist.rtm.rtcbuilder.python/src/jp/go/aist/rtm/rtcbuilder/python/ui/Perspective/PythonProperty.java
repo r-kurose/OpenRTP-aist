faacd097682933dd4a0f91e86056b9eeaefcdbdd
openrtm <openrtm@openrtm.org>
2008-09-12 08:27:25 +0000

    The first import.
    git-svn-id: https://tmp.openrtm.org/svn/rtmtools/trunk@9 16521fb8-224f-4d59-aec7-8b1ded20d512

---
+++ rtcbuilder/trunk/jp.go.aist.rtm.rtcbuilder.python/src/jp/go/aist/rtm/rtcbuilder/python/ui/Perspective/PythonProperty.java
@@ -0,0 +1,31 @@
+package jp.go.aist.rtm.rtcbuilder.python.ui.Perspective;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.go.aist.rtm.rtcbuilder.ui.Perspective.LanguageProperty;
+
+public class PythonProperty extends LanguageProperty {
+	private String PerspectiveId = "org.python.pydev.ui.PythonPerspective";
+	private String PerspectiveName = "Python";
+	private String PluginId = "org.python.pydev";
+
+	public String getPerspectiveId() {
+		return PerspectiveId;
+	}
+
+	public String getPerspectiveName() {
+		return PerspectiveName;
+	}
+
+	public String getPluginId() {
+		return PluginId;
+	}
+
+	@Override
+	public List<String> getNatures() {
+		List<String> natures = new ArrayList<String>();
+		natures.add("org.python.pydev.pythonNature");
+		return natures;
+	}
+}
