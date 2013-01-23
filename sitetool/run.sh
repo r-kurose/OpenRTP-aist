a5567d92d1e3cd53a9467bb9ec2e1d72e35f0d11
n-ando <Noriaki.Ando@gmail.com>
2013-01-23 15:38:03 +0000

    run.sh now find some files and set env value itself
    git-svn-id: https://tmp.openrtm.org/svn/rtmtools/trunk@379 16521fb8-224f-4d59-aec7-8b1ded20d512

--- sitetool/run.sh
+++ sitetool/run.sh
@@ -3,10 +3,56 @@
 cd `dirname $0`
 pwd
 
-export ANT_HOME=eclipse/plugins/org.apache.ant_1.7.0.v200803061910/
+
+if test "x$ECLIPSE_HOME" = "x" ; then
+    echo "Environment variable ECLIPSE_HOME is not set. Aborting."
+    return 1
+fi
+if test ! -d $ECLIPSE_HOME ; then
+    echo "ECLIPSE_HOME $ECLIPSE_HOME does not exist. Aborting."
+    return 1
+fi
+
+
+ANT_HOME="${ECLIPSE_HOME}/plugins/org.apache.ant_1.7.0.v200803061910/"
+
+if test ! -d $ANT_HOME ; then
+    echo "ANT_HOME $ANT_HOME does not exist. Finding another ant plugin."
+    other_ant=`find $ECLIPSE_HOME/plugins -maxdepth 1 -type d -name 'org.apache.ant*'`
+    echo $other_ant
+    if test "x$other_ant" = "x" ; then
+        echo "Ant plugin was not found. Aborting."
+        return 1
+    fi
+    ANT_HOME=$other_ant
+fi
+
+if test "x$PLUGINS_DIR" = "x" ; then
+    echo "Environment variable PLUGINS_DIR is not set."
+    tmp=`find ../ -name 'jp.go.aist.rtm.toolscommon.nl*.jar' ! -name '*feature*' -exec dirname {} \;`
+    if test "x$tmp" = "x" ; then
+        echo "Plugins not found. Trying to build plugins."
+        cd ../
+        sh buildall.sh
+        if test $? -ne 0 ; then
+            echo "Build failed. Aborting."
+            return 1
+        fi
+        tmp=`find ../ -name 'jp.go.aist.rtm.toolscommon.nl*.jar' ! -name '*feature*' -exec dirname {} \;`
+        if test "x$tmp" = "x" ; then
+            echo "Plugins not found. Aborting."
+            return 1
+        fi
+    fi
+    PLUGINS_DIR=$tmp
+fi
+
+export ANT_HOME
+export PLUGINS_DIR
 export PATH=${PATH}:${ANT_HOME}/bin
 
 echo "ANT_HOME=${ANT_HOME}"
+echo "PLUGINS_DIR=${PLUGINS_DIR}"
 echo "PATH=${PATH}"
 
 ant -lib lib get.plugins
