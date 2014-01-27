bd06f9b0227438917287a75d13539af32eddc4e4
win-ei <openrtm@openrtm.org>
2014-01-27 07:19:23 +0000

    Added TemplateUtil.java and GeneratedResult.java to jp/go/aist/rtm/toolscommon/uti.  Made a change accompanying it.
    git-svn-id: https://tmp.openrtm.org/svn/rtmtools/branches@502 16521fb8-224f-4d59-aec7-8b1ded20d512

--- work_ForRTMSafety/rtmtools/jp.go.aist.rtm.rtcbuilder.python/src/jp/go/aist/rtm/rtcbuilder/python/manager/PythonGenerateManager.java
+++ work_ForRTMSafety/rtmtools/jp.go.aist.rtm.rtcbuilder.python/src/jp/go/aist/rtm/rtcbuilder/python/manager/PythonGenerateManager.java
@@ -6,14 +6,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import jp.go.aist.rtm.rtcbuilder.generator.GeneratedResult;
+//import jp.go.aist.rtm.rtcbuilder.generator.GeneratedResult;
+import jp.go.aist.rtm.toolscommon.util.GeneratedResult;
 import jp.go.aist.rtm.rtcbuilder.generator.param.RtcParam;
 import jp.go.aist.rtm.rtcbuilder.generator.param.idl.IdlFileParam;
 import jp.go.aist.rtm.rtcbuilder.generator.param.idl.ServiceClassParam;
 import jp.go.aist.rtm.rtcbuilder.manager.GenerateManager;
 import jp.go.aist.rtm.rtcbuilder.python.ui.Perspective.PythonProperty;
 import jp.go.aist.rtm.rtcbuilder.template.TemplateHelper;
-import jp.go.aist.rtm.rtcbuilder.template.TemplateUtil;
+//import jp.go.aist.rtm.rtcbuilder.template.TemplateUtil;
+import jp.go.aist.rtm.toolscommon.util.TemplateUtil;
 import jp.go.aist.rtm.rtcbuilder.ui.Perspective.LanguageProperty;
 
 import static jp.go.aist.rtm.rtcbuilder.IRtcBuilderConstants.*;
