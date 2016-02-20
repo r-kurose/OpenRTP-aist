0f831068e9b672fdd17f1039a86b5de5413967fc
ga <openrtm@openrtm.org>
2016-02-20 17:37:01 +0000

    Modified Logger #3211
    git-svn-id: https://tmp.openrtm.org/svn/rtmtools/trunk@543 16521fb8-224f-4d59-aec7-8b1ded20d512

--- jp.go.aist.rtm.rtcbuilder.python/src/jp/go/aist/rtm/rtcbuilder/python/RtcBuilderPythonPlugin.java
+++ jp.go.aist.rtm.rtcbuilder.python/src/jp/go/aist/rtm/rtcbuilder/python/RtcBuilderPythonPlugin.java
@@ -1,16 +1,19 @@
 package jp.go.aist.rtm.rtcbuilder.python;
 
-import java.util.logging.Logger;
-
-import jp.go.aist.rtm.rtcbuilder.RtcBuilderPlugin;
+import jp.go.aist.rtm.toolscommon.profiles.util.LoggerUtil;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class RtcBuilderPythonPlugin extends AbstractUIPlugin {
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(RtcBuilderPythonPlugin.class);
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "jp.go.aist.rtm.rtcbuilder.python";
@@ -22,8 +25,10 @@ public class RtcBuilderPythonPlugin extends AbstractUIPlugin {
 	 * The constructor
 	 */
 	public RtcBuilderPythonPlugin() {
+		LoggerUtil.setup();
+		LOGGER.trace("RtcBuilderPythonPlugin: START");
+
 		plugin = this;
-		getLogger();
 	}
 
 	/*
@@ -31,8 +36,6 @@ public class RtcBuilderPythonPlugin extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		RtcBuilderPlugin.addLogger(getLogger());
-		//
 		super.start(context);
 	}
 
@@ -41,8 +44,6 @@ public class RtcBuilderPythonPlugin extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		RtcBuilderPlugin.removeLogger(getLogger());
-		//
 		plugin = null;
 		super.stop(context);
 	}
@@ -54,15 +55,6 @@ public class RtcBuilderPythonPlugin extends AbstractUIPlugin {
 	 */
 	public static RtcBuilderPythonPlugin getDefault() {
 		return plugin;
-	}
-
-	static Logger log;
-
-	public static Logger getLogger() {
-		if (log == null) {
-			log = Logger.getLogger(PLUGIN_ID);
-		}
-		return log;
 	}
 
 }
