814522e0e38d6af1c540717af279a9a3589f8338
Sakamoto Takeshi <sakamoto@globalassist.co.jp>
2019-07-26 01:19:59 +0900

    Modified unit tests.#116

--- jp.go.aist.rtm.rtcbuilder/test/jp/go/aist/rtm/rtcbuilder/_test/TestBase.java
+++ jp.go.aist.rtm.rtcbuilder/test/jp/go/aist/rtm/rtcbuilder/_test/TestBase.java
@@ -16,7 +16,7 @@ public class TestBase extends TestCase {
 	protected String expPath;
 	protected String expContent;
 	protected int index;
-	protected final int default_file_num = 34;
+	protected final int default_file_num = 36;
 
 	public TestBase () {
 		File fileCurrent = new File(".");
