d10f54fb1aa2cdce34b1ce0411c1823361134d1d
Sakamoto Takeshi <sakamoto@globalassist.co.jp>
2019-11-07 23:57:43 +0900

    Modified unit tests.#230

--- jp.go.aist.rtm.rtcbuilder.python/resource/100/aist/AIST2/idlcompile.bat
+++ jp.go.aist.rtm.rtcbuilder.python/resource/100/aist/AIST2/idlcompile.bat
@@ -1,12 +1,2 @@
 @echo off
-cd /d %~dp0
-setlocal
-for %%I in (python.exe) do if exist %%~$path:I set f=%%~$path:I
-if exist %f% (
-  @echo off
-  omniidl.exe -bpython -I"%RTM_ROOT%\rtm\idl" idl/MyService.idl 
-) else (
-  echo "python.exe" can not be found.
-  echo Please modify PATH environmental variable for python command.
-)
-endlocal
+omniidl.exe -bpython -I"%RTM_ROOT%\rtm\idl" idl/MyService.idl 
